951d2ecbff75a21637e237129e50bc69704f7350
matt knox <matthewknox@gmail.com>
2012-01-05 21:13:47 -0800

    as expected, functional tends to be shorter, even when I am dranked.

--- 2012_01_05_deluxe_functional/test/test_conway.rb
+++ 2012_01_05_deluxe_functional/test/test_conway.rb
@@ -1,47 +1,56 @@
 require "rubygems"
 require "test/unit"
 
-def evolve(live_cells)
-  neighbor_counts = Hash.new(0)
+def make_evolver(live_f, neighbor_f)
 
-  live_cells.each do |cell|
-    neighbors(cell).each do |c|
-      neighbor_counts[c] += 1
+
+  def evolve(live_f, neighbor_f, live_cells)
+    neighbor_counts = Hash.new(0)
+
+    live_cells.each do |cell|
+      neighbor_f.call(cell).each do |c|
+        neighbor_counts[c] += 1
+      end
     end
+
+    neighbor_counts.map { |k, v| live_f.call(k, v, live_cells)}.compact.sort
   end
 
-  neighbor_counts.map { |k, v| live?(k, v, live_cells)}.compact.sort
+  lambda { |live_cells| evolve(live_f, neighbor_f, live_cells) }
 end
 
-def live?(cell, neighbor_count, live_cells)
+LIVE = lambda { |cell, neighbor_count, live_cells|
   cell if neighbor_count == 3 || ( neighbor_count == 2 && live_cells.member?(cell))
-end
+}
 
-def neighbors(cell)
-  [[-1, -1], [-1, 0], [-1, 1],
-   [0, -1], [0, 1],
-   [1, -1], [1, 0], [1, 1]].map { |x, y| [cell.first + x, cell.last + y]}
-end
+NEIGHBORS = lambda { |cell| [[-1, -1], [-1, 0], [-1, 1],
+                             [0, -1], [0, 1],
+                             [1, -1], [1, 0], [1, 1]].map { |x, y|
+    [cell.first + x, cell.last + y]} }
 
 class ConwayTest < Test::Unit::TestCase
+  def setup
+    @evolver = make_evolver(LIVE, NEIGHBORS)
+  end
+
   SQUARE = [[1, 1], [1, 2], [2, 1], [2, 2]]
   LONELY = [[1, 1]]
   H_SEMAPHORE = [[1, 1], [1, 2], [1, 3]]
   V_SEMAPHORE = [[0, 2], [1, 2], [2, 2]]
 
   def test_empty_board
-    assert_equal [], evolve([])
+    assert_equal [], @evolver.call([])
   end
 
   def test_lonely_board
-    assert_equal [], evolve([[1,1]])
+    assert_equal [], @evolver.call([[1,1]])
   end
   def test_square
-    assert_equal SQUARE, evolve(SQUARE)
+    assert_equal SQUARE, @evolver.call(SQUARE)
   end
 
   def test_semaphore
-    assert_equal H_SEMAPHORE, evolve(V_SEMAPHORE)
-    assert_equal V_SEMAPHORE, evolve(H_SEMAPHORE)
+    assert_equal H_SEMAPHORE, @evolver.call(V_SEMAPHORE)
+    assert_equal V_SEMAPHORE, @evolver .call(H_SEMAPHORE)
   end
 end
