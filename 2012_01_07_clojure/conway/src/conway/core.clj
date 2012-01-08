f3ecb8343850e75fd950c86f37294f3faf32c34b
matt knox <matthewknox@gmail.com>
2012-01-07 20:33:23 -0800

    started clojure version

---
+++ 2012_01_07_clojure/conway/src/conway/core.clj
@@ -0,0 +1,1 @@
+(ns conway.core)
---
+++ 2012_01_07_clojure/conway/test/conway/test/core.clj
@@ -0,0 +1,6 @@
+(ns conway.test.core
+  (:use [conway.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
