3ce8d6deb968090b5a83bc97bebbaa20ffc000aa
matt knox <matthewknox@gmail.com>
2012-01-08 15:02:47 -0800

    more work

--- 2012_01_07_clojure/conway/test/conway/test/core.clj
+++ 2012_01_07_clojure/conway/test/conway/test/core.clj
@@ -1,6 +1,25 @@
 (ns conway.test.core
+  (:require clojure.contrib.combinatorics)
   (:use [conway.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(def square #{[2 1] [2 2] [1 1] [1 2]})
+
+(defn evolve [live_cells] live_cells)
+
+(defn neighbors [[cell-x cell-y]]
+  (map (fn [[x y]] [(+ cell-x x) (+ cell-y y)])
+       [[-1 -1] [-1 0] [-1 1]
+        [0 -1] [0 1]
+        [1 -1] [1 0] [1 1]]))
+
+
+
+(deftest empty-board-is-stable
+  (is #{} ( evolve #{})))
+
+(deftest square-board-is-stable
+  (is square (evolve square)))
+
+(deftest lonely-board-dies
+  (is #{} (evolve #{[1 1]})))
