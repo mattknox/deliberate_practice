e746efe322176bf7349baa8bde1e33f5f8a9c9e9
matt knox <matthewknox@gmail.com>
2012-01-08 23:27:37 -0800

    working version in clojure

--- 2012_01_07_clojure/conway/test/conway/test/core.clj
+++ 2012_01_07_clojure/conway/test/conway/test/core.clj
@@ -5,21 +5,33 @@
 
 (def square #{[2 1] [2 2] [1 1] [1 2]})
 
-(defn evolve [live_cells] live_cells)
-
 (defn neighbors [[cell-x cell-y]]
   (map (fn [[x y]] [(+ cell-x x) (+ cell-y y)])
        [[-1 -1] [-1 0] [-1 1]
         [0 -1] [0 1]
         [1 -1] [1 0] [1 1]]))
 
-
+(defn evolve [live-cells]
+  (let [neighbor-counts (reduce (fn [x y] (conj x [y (inc (or (x y) 0))]))
+                                {}
+                                (reduce into
+                                        '()
+                                        (map (fn [x] (neighbors x))
+                                             live-cells)))]
+    (set (filter (fn [c] (or (= 3 (neighbor-counts c))
+                            (and (= 2 (neighbor-counts c)) (live-cells c))))
+                 (keys (reduce (fn [x y] (conj x [y (inc (or (x y) 0))]))
+                               {}
+                               (reduce into
+                                       #{}
+                                       (map (fn [x] (neighbors x))
+                                            live-cells))))))))
 
 (deftest empty-board-is-stable
-  (is #{} ( evolve #{})))
+  (is (= #{} (evolve #{}))))
 
 (deftest square-board-is-stable
-  (is square (evolve square)))
+  (is (= square (evolve square))))
 
 (deftest lonely-board-dies
-  (is #{} (evolve #{[1 1]})))
+  (is (= #{} (evolve #{[1 1]}))))
