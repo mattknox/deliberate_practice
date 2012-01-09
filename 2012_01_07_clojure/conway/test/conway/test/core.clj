9905b6ba2a626ec40875e8cb02a6d0ed6d9940b3
matt knox <matthewknox@gmail.com>
2012-01-08 23:52:32 -0800

    style changes

--- 2012_01_07_clojure/conway/test/conway/test/core.clj
+++ 2012_01_07_clojure/conway/test/conway/test/core.clj
@@ -1,31 +1,36 @@
 (ns conway.test.core
-  (:require clojure.contrib.combinatorics)
   (:use [conway.core])
   (:use [clojure.test]))
-
-(def square #{[2 1] [2 2] [1 1] [1 2]})
 
 (defn neighbors [[cell-x cell-y]]
   (map (fn [[x y]] [(+ cell-x x) (+ cell-y y)])
        [[-1 -1] [-1 0] [-1 1]
-        [0 -1] [0 1]
-        [1 -1] [1 0] [1 1]]))
+        [0 -1]         [0 1]
+        [1 -1]  [1 0]  [1 1]]))
 
-(defn evolve [live-cells]
-  (let [neighbor-counts (reduce (fn [x y] (conj x [y (inc (or (x y) 0))]))
+(defn count-neighbors [live-cells]
+  (reduce (fn [x y] (conj x [y (inc (or (x y) 0))]))
                                 {}
                                 (reduce into
                                         '()
                                         (map (fn [x] (neighbors x))
-                                             live-cells)))]
-    (set (filter (fn [c] (or (= 3 (neighbor-counts c))
-                            (and (= 2 (neighbor-counts c)) (live-cells c))))
+                                             live-cells))))
+
+(defn live? [cell live-neighbor-count live-cells]
+  (or (= 3 live-neighbor-count)
+      (and (= 2 live-neighbor-count) (live-cells cell))))
+
+(defn evolve [live-cells]
+  (let [neighbor-counts (count-neighbors live-cells)]
+    (set (filter (fn [c] (live? c (neighbor-counts c) live-cells))
                  (keys (reduce (fn [x y] (conj x [y (inc (or (x y) 0))]))
                                {}
                                (reduce into
                                        #{}
                                        (map (fn [x] (neighbors x))
                                             live-cells))))))))
+
+(def square #{[2 1] [2 2] [1 1] [1 2]})
 
 (deftest empty-board-is-stable
   (is (= #{} (evolve #{}))))
