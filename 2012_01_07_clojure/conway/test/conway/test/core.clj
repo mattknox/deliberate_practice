83b59a72211c77442ee9f1f6c523e4386dfdec19
matt knox <matthewknox@gmail.com>
2012-01-09 09:21:06 -0800

    moved code out of test file

--- 2012_01_07_clojure/conway/test/conway/test/core.clj
+++ 2012_01_07_clojure/conway/test/conway/test/core.clj
@@ -2,34 +2,6 @@
   (:use [conway.core])
   (:use [clojure.test]))
 
-(defn neighbors [[cell-x cell-y]]
-  (map (fn [[x y]] [(+ cell-x x) (+ cell-y y)])
-       [[-1 -1] [-1 0] [-1 1]
-        [0 -1]         [0 1]
-        [1 -1]  [1 0]  [1 1]]))
-
-(defn count-neighbors [live-cells]
-  (reduce (fn [x y] (conj x [y (inc (or (x y) 0))]))
-                                {}
-                                (reduce into
-                                        '()
-                                        (map (fn [x] (neighbors x))
-                                             live-cells))))
-
-(defn live? [cell live-neighbor-count live-cells]
-  (or (= 3 live-neighbor-count)
-      (and (= 2 live-neighbor-count) (live-cells cell))))
-
-(defn evolve [live-cells]
-  (let [neighbor-counts (count-neighbors live-cells)]
-    (set (filter (fn [c] (live? c (neighbor-counts c) live-cells))
-                 (keys (reduce (fn [x y] (conj x [y (inc (or (x y) 0))]))
-                               {}
-                               (reduce into
-                                       #{}
-                                       (map (fn [x] (neighbors x))
-                                            live-cells))))))))
-
 (def square #{[2 1] [2 2] [1 1] [1 2]})
 
 (deftest empty-board-is-stable
