78e5ff4b1f496d0bc56c82ade2855e9460ccd57e
matt knox <matthewknox@gmail.com>
2012-01-12 00:32:29 -0800

    got some tests and infra code written.  JS is hecka unpleasant.

---
+++ 2012_01_12_node/test/test.js
@@ -0,0 +1,38 @@
+var assert = require("assert");
+
+function evolve(liveCells) {
+  return liveCells;
+}
+
+function equal(cellSet1, cellSet2) {
+  if (cellSet1.length != cellSet2.length) {
+    return false;
+  } else {
+    for(var i = 0; i < cellSet1.length; i++) {
+      if (cellSet1[i] != cellSet2[i]){
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+exports['equal([], []) => true'] = function(){
+  assert.equal(true, equal([], []));
+};
+
+exports['equal([1], [1] => true'] = function(){
+  assert.equal(true, equal([1], [1]));
+};
+
+exports['equal([1], [1, 2] => false'] = function(){
+  assert.equal(false, equal([1], [1, 2]));
+};
+
+exports['equal([1, 2], [1, 2] => true'] = function(){
+  assert.equal(true, equal([1,2], [1, 2]));
+};
+
+exports['empty board is stable'] = function(){
+  assert.equal(true, equal([], evolve([])));
+};
