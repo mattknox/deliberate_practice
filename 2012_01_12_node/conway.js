2dac4a7789af5d788340198aa50057291f0c25ab
matt knox <matthewknox@gmail.com>
2012-01-16 15:30:43 -0800

    working js solution to conway.  Horrid little language.

--- 2012_01_12_node/conway.js
+++ 2012_01_12_node/conway.js
@@ -1,24 +1,60 @@
-exports.evolve = function(liveCells) {
-  return liveCells;
-}
+var util = require("util");
 
-function equal(cellSet1, cellSet2) {
+exports.evolve = function(liveCells) {
+  var nextGen = [];
+  var liveMap = {};
+  var neighborCounts = {};
+  for(var i = 0; i < liveCells.length; i++) {
+    liveMap[liveCells[i].join(",")] = 1;
+    var neighborCells = this.neighbors(liveCells[i]);
+    for (var j = 0; j < neighborCells.length; j++) {
+      var index = neighborCells[j].join(",");
+      neighborCounts[index] = (neighborCounts[index] || 0) + 1;
+    }
+  }
+
+  for( k in neighborCounts){
+    if( neighborCounts[k] == 3 || ((neighborCounts[k] == 2) && liveMap[k])) {
+      nextGen.push(explode(k));
+    }
+  }
+  return nextGen.sort();
+};
+
+function explode(str) {
+  var ret = str.split(",");
+  for (var i = 0; i < ret.length; i++) {
+    ret[i] = Number(ret[i]);
+  }
+  return ret;
+}
+exports.neighbors = function(cell) {
+  var indices = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+  var arr = [];
+  for (var i = 0; i < 8; i++){
+    var c = [cell[0] + indices[i][0], cell[1] + indices[i][1]]; 
+    arr.push(c);
+  }
+  return arr;
+};
+
+function equal(o1, o2) {
   // js has a retarded == operator, which says [] != []
   // this operator does structural equality, so that two
   // arrays that print the same will be equal.
   if ([].constructor != Array && [].constructor != Object) {
-    return cellSet1 == cellSet2;
-  } else if (cellSet1.length != cellSet2.length) {
+    return o1 == o2;
+  } else if (o1.length != o2.length) {
     return false;
   } else if ([].constructor === Array){
-    for(var i = 0; i < cellSet1.length; i++) {
-      if (!equal(cellSet1[i], cellSet2[i])){
+    for(var i = 0; i < o1.length; i++) {
+      if (!equal(o1[i], o2[i])){
         return false;
       }
     }
-  } else if (cellSet1.constructor === Object) {
-    for(k in cellSet1) {
-      if (!equal(cellSet1[k], cellSet2[k])) {
+  } else if (o1.constructor === Object) {
+    for(k in o1) {
+      if (!equal(o1[k], o2[k])) {
         return false;
       }
     }
--- 2012_01_12_node/test/test.js
+++ 2012_01_12_node/test/test.js
@@ -1,35 +1,6 @@
 var assert = require("assert");
 var conway = require("../conway.js");
-
-// function evolve(liveCells) {
-//   return liveCells;
-// }
-
-// function equal(cellSet1, cellSet2) {
-//   // js has a retarded == operator, which says [] != []
-//   // this operator does structural equality, so that two
-//   // arrays that print the same will be equal.
-//   if ([].constructor != Array && [].constructor != Object) {
-//     return cellSet1 == cellSet2;
-//   } else if (cellSet1.length != cellSet2.length) {
-//     return false;
-//   } else if ([].constructor === Array){
-//     for(var i = 0; i < cellSet1.length; i++) {
-//       if (!equal(cellSet1[i], cellSet2[i])){
-//         return false;
-//       }
-//     }
-//   } else if (cellSet1.constructor === Object) {
-//     for(k in cellSet1) {
-//       if (!equal(cellSet1[k], cellSet2[k])) {
-//         return false;
-//       }
-//     }
-//   } else {
-//     return false; // don't know what to do with other types
-//   }
-//   return true;
-// }
+var util = require("util");
 
 exports['equal([], []) => true'] = function(){
   assert.equal(true, conway.equal([], []));
@@ -56,10 +27,11 @@ exports['empty board is stable'] = function(){
 };
 
 exports['empty square is stable'] = function(){
-  assert.equal(true, conway.equal([[1,1], [1,2], [2,1], [2,2]],
-                           conway.evolve([[1,1], [1,2], [2,1], [2,2]])));
+  assert.equal(true, conway.equal([[1,1], [1,2], [2,1], [2,2]].sort(),
+                                  conway.evolve([[1,1], [1,2], [2,1], [2,2]])));
 };
 
 exports['lonely board dies'] = function(){
   assert.equal(true, conway.equal([], conway.evolve([[1,1]])));
 };
+
