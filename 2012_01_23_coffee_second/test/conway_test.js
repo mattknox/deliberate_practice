15cbe2207ab95b23365a50002c4d7868c7633bd6
matt knox <matthewknox@gmail.com>
2012-01-23 23:16:00 -0800

    tests green.  Working now.

---
+++ 2012_01_23_coffee_second/test/conway_test.js
@@ -0,0 +1,35 @@
+var assert = require("assert");
+var conway = require("../conway.js");
+
+exports['equal([], []) => true'] = function(){
+  assert.equal(true, conway.equal([], []));
+};
+
+exports['equal([1], [1] => true'] = function(){
+  assert.equal(true, conway.equal([1], [1]));
+};
+
+exports['equal([1], [1, 2] => false'] = function(){
+  assert.equal(false, conway.equal([1], [1, 2]));
+};
+
+exports['equal([1, 2], [1, 2] => true'] = function(){
+  assert.equal(true, conway.equal([1,2], [1, 2]));
+};
+
+exports['equal([[1], [2]], [[1], [2]] => true'] = function(){
+  assert.equal(true, conway.equal([[1],[2]], [[1], [2]]));
+};
+
+exports['empty board is stable'] = function(){
+  assert.equal(true, conway.equal([], conway.evolve([])));
+};
+
+exports['empty square is stable'] = function(){
+  assert.equal(true, conway.equal([[1,1], [1,2], [2,1], [2,2]].sort(),
+                                  conway.evolve([[1,1], [1,2], [2,1], [2,2]])));
+};
+
+exports["lonely board dies"] = function () {
+  assert.equal(true, conway.equal([], conway.evolve([[1,1]])));
+};
