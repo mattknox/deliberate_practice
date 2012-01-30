4adce42255a000578a598b2e14e3c7a4181f012e
matt knox <matthewknox@gmail.com>
2012-01-29 21:01:52 -0800

    set for rewrite

---
+++ 2012_01_29_clojure_second/conway/src/conway/core.clj
@@ -0,0 +1,31 @@
+(ns conway.core
+  (:use clojure.contrib.combinatorics))
+
+(defn- neighbors [[cell-x cell-y]]
+  (map (fn [[x y]] [(+ cell-x x) (+ cell-y y)])
+       [[-1 -1] [-1 0] [-1 1]
+        [0 -1]         [0 1]
+        [1 -1]  [1 0]  [1 1]]))
+
+(defn- count-neighbors [live-cells]
+  (reduce (fn [x y] (conj x [y (inc (or (x y) 0))]))
+                                {}
+                                (reduce into
+                                        '()
+                                        (map (fn [x] (neighbors x))
+                                             live-cells))))
+
+(defn- live? [cell live-neighbor-count live-cells]
+  (or (= 3 live-neighbor-count)
+      (and (= 2 live-neighbor-count) (live-cells cell))))
+
+(defn evolve [live-cells]
+  (let [neighbor-counts (count-neighbors live-cells)]
+    (set (filter (fn [c] (live? c (neighbor-counts c) live-cells))
+                 (keys (reduce (fn [x y] (conj x [y (inc (or (x y) 0))]))
+                               {}
+                               (reduce into
+                                       #{}
+                                       (map (fn [x] (neighbors x))
+                                            live-cells))))))))
+
