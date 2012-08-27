37a35ac2393ac02f3938921f4015fbf08af897bd
matt knox <matthewknox@gmail.com>
2012-08-27 16:36:06 -0700

    added new stuff

--- strstr/strstr.rb
+++ strstr/strstr.rb
@@ -19,3 +19,10 @@ assert -1, "foo", "bar"
 assert 0, "foo", "foo"
 assert 3, "foo", "barfoo"
 puts
+
+
+def strstr_rec(n, h, i = 0, j = 0)
+  return i if j == n.size
+  return -1 if i == h.size
+  n[j] == h[i + j] ? strstr_rec(n, h, i, j + 1) : strstr_rec(n, h, i + 1, 0)
+end
