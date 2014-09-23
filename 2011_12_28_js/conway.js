c1acfae51e0787a793fe047dbc44b1fc8a6a336f
matt knox <mknox@twitter.com>
2014-09-22 18:02:40 -0700

    moved the conway stuff into its own dir

--- 2011_12_28_js/conway.js
+++
@@ -1,8 +0,0 @@
-(function () {
-   this.passed = true;
-   function assert(desc, test_function) {
-     var result = test_function();
-     passed = passed && result;
-     return result && console.info(desc);
-   }
- })();
