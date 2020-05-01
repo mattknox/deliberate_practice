41407e244ae5376e5448614f14791144454f0d26
matt knox <matthewknox@gmail.com>
2020-05-01 15:37:25 -0700

    ruby again, this time as a module

--- tokenize/2020_05_01_oo.rb
+++ tokenize/2020_05_01_oo.rb
@@ -6,3 +6,22 @@
     { kind: "hashtag", text: "#typo", start: 35, length: 5}
   ]
 }
+
+module Tokenizer
+  DELIMITERS = {
+    "@": "mention",
+    "#": "hashtag",
+  }
+
+  SPLITTER = /([#@]\w+)/ # TODO: validations on hashtag or mention length?
+
+  def self.tokenize(input)
+    spans = []
+    offset = 0
+    input.split(SPLITTER).each do |s|
+      spans << { kind: s[0].to_sym, text: s, start: offset, length: s.length } if s.match(SPLITTER)
+      offset += s.length
+    end
+    {raw: input, spans: spans}
+  end
+end
